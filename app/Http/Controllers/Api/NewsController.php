734702f07d8cdb034df4cacf6a9c78a42185732f
18011354121 <979995356@qq.com>
2021-05-26 20:26:31 +0800

    'new'

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\BaseController;
 use App\Http\Model\News;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class NewsController extends BaseController
@@ -17,32 +18,46 @@ class NewsController extends BaseController
     }
 
 
+    /**
+     * @param News $news
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function get_news_contents(News $news)
     {
+        #http://www.coindog.com/type/jinse/lives
         $accessKey = 'a15badd5fd7418f6ad49629792d4a51b';
         $secretKey = '5687d270949a5acc';
         $httpParams = array(
             'access_key' => $accessKey,
             'date' => time()
         );
-
         $signParams = array_merge($httpParams, array('secret_key' => $secretKey));
-
         ksort($signParams);
         $signString = http_build_query($signParams);
-
         $httpParams['sign'] = strtolower(md5($signString));
-
-        $url = 'http://api.coindog.com/topic/list?' . http_build_query($httpParams);
+        $url = 'http://api.coindog.com/live/list?' . http_build_query($httpParams);
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
         $curlRes = curl_exec($ch);
         curl_close($ch);
-
         $json = json_decode($curlRes, true);
-        return $this->_success($json);
+
+        if (empty($json)) return false;
+        $data_all = [];
+        foreach ($json['list'][0]['lives'] as $k => &$v) {
+            preg_match('/【(.*?)】/is', $v['content'], $result);
+            $data = array(
+                'nid'=>1,
+                'news_id' => $v['id'],
+                'content' => $v['content'],
+                'time' => $v['created_at'],
+                'title' => $result[1] ?? '',
+            );
+
+            array_push($data_all, $data);
+        }
+        DB::table($news->getTable())->insert($data_all);
     }
 
 }
